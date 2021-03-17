62f83876d249d1130378a621f53d3fe0b5f67c1e
Romain Chardiny <romain.chardiny@gmail.com>
2021-03-17 18:22:12 +0000

    fix bug

--- algo.py
+++ algo.py
@@ -3,10 +3,18 @@ def create_dict(nodes, relations):
     for node in nodes:
         id = node[0]
         neighbours = []
-        for start, end in relations:
+        for relation in relations:
+            start = relation[0]
+            end = relation[1]
             if start == id:
                 neighbours.append(end)
             elif end == id:
                 neighbours.append(start)
         result[id] = neighbours
     return result
+
+n = [[1, "name"], [2, "test"]]
+
+r = [[1, 2, True, 6]]
+
+print(create_dict(n, r))
