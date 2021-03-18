6a63f057e07797f46db5851123a8e5944095e7b3
Romain Chardiny <romain.chardiny@gmail.com>
2021-03-18 18:28:17 +0100

    parcours

--- algo.py
+++ algo.py
@@ -13,8 +13,39 @@ def create_dict(nodes, relations):
         result[id] = neighbours
     return result
 
-n = [[1, "name"], [2, "test"]]
 
-r = [[1, 2, True, 6]]
+def breadth_first_search(start_node, grap_dict):
+    """
+    Parcours en largeur
+    """
+    if not start_node in grap_dict.keys():
+        raise RuntimeError("Node not in graph")
+    stack = []
+    already_crossed = []
+    stack.append(start_node)
+    while len(stack) != 0:
+        s = stack[0]
+        for neighbour in grap_dict[s]:
+            if not neighbour in already_crossed and not neighbour in stack:
+                stack.append(neighbour)
+        stack.pop(0)
+        already_crossed.append(s)
+    return already_crossed
 
-print(create_dict(n, r))
+
+def depth_first_search(start_node, graph_dict):
+    """
+    Parcours en profondeur
+    """
+    if not start_node in graph_dict.keys():
+        raise RuntimeError("Node not in graph")
+    queue = []
+    already_crossed = []
+    queue.append(start_node)
+    while len(queue) != 0:
+        s = queue.pop()
+        for neighbour in graph_dict[s]:
+            if not neighbour in queue and not neighbour in already_crossed:
+                queue.append(neighbour)
+        already_crossed.append(s)
+    return already_crossed
