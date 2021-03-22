783ac9e6bd8882a25e5a1e5f5ad054000f6d9610
Romain C <38137329+romch007@users.noreply.github.com>
2021-03-22 11:25:43 +0100

    grga

--- static/main.js
+++ static/main.js
@@ -1,3 +1,5 @@
+const nodes = new Map();
+
 const stage = new Konva.Stage({
   container: "container",
   width: 1000,
@@ -8,34 +10,28 @@ const layer = new Konva.Layer();
 
 stage.add(layer);
 
-const alpha = "abcdefghijklmnopqrstuvwxyz";
-
-function idToString(id) {
-  const chars = [];
-  for (let i = 0; i < id; i++) {
-    const pos = i % alpha.length;
-    chars.push(alpha[pos]);
-  }
-  return chars.join("");
-}
 
 function drawNodeCircle(id, name, x, y) {
-  const group = new Konva.Group({ x, y, draggable: true, id: idToString(id) });
+  const group = new Konva.Group({ x, y, draggable: true });
   const circle = new Konva.Circle({
-    x,
-    y,
     radius: 40,
     fill: "grey",
     stroke: "black",
     strokeWidth: 2
   });
   const text = new Konva.Text({
-    x,
-    y,
     text: name,
     fontSize: 15,
-    fontFamily: "Arial"
+    fontFamily: "Arial",
+    align: "center",
+    verticalAlign: "middle",
+   	width: circle.radius(),
+    height: circle.radius(),
+    offsetX: circle.radius() / 2,
+    offsetY: circle.radius() / 2
   });
+  
+  nodes.set(id, group);
 
   group.add(circle);
   group.add(text);
@@ -44,21 +40,39 @@ function drawNodeCircle(id, name, x, y) {
 }
 
 function drawRelation(startId, endId) {
-  const startCircle = stage.find(`#${idToString(startId)}`)[0];
-  const endCircle = stage.find(`#${idToString(endId)}`)[0];
-
-  console.log(startCircle);
+  const startNode = nodes.get(startId);
+  const endNode = nodes.get(endId);
 
   const line = new Konva.Line({
-    points: [startCircle.x(), startCircle.y(), endCircle.x(), endCircle.y()],
+    points: [startNode.x(), startNode.y(), endNode.x(), endNode.y()],
     stroke: "black",
-    strokeWidth: 3
+    strokeWidth: 3,
+
   });
   layer.add(line);
+  line.moveToBottom();
+  
+  startNode.on("dragmove", event => {
+     line.points([
+     		startNode.x(),
+        startNode.y(),
+        line.points()[2],
+        line.points()[3]
+     ]);
+     //layer.draw()
+  });
+  endNode.on("dragmove", event => {
+  	line.points([
+     		line.points()[0],
+        line.points()[1],
+        endNode.x(),
+        endNode.y()
+     ]);
+  });
 }
 
-drawNodeCircle(1, "test", 100, 100);
-drawNodeCircle(2, "saalut", 300, 300);
+drawNodeCircle(1, "A", 100, 100);
+drawNodeCircle(2, "B", 300, 300);
 
 // drawRelation();
 
